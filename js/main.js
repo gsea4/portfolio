0d8d94b1946a1a7cbaca0f9a07c18464672f7481
HaiH <haihuynhkbn@gmail.com>
2017-08-29 00:35:11 -0500

    Added one more project section/ Fixed <hr> display

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
     $(window).resize(function(){
         if($(window).width() < 768){
             $(".featurette").css("display", "none");
+            $(".featurette-divider").css("display", "none");
             $("#myCarousel").css("display", "block");
         }else{
             $(".featurette").css("display", "block");
+            $(".featurette-divider").css("display", "block");
             $("#myCarousel").css("display", "none");
         }
     });
