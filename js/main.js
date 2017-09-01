426e73917a437c04dc5ad15471ec7692888c3b7d
HaiH <haihuynhkbn@gmail.com>
2017-09-01 02:12:53 -0500

    Fixed animation when click on Projects and Contact from the About section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
     
+    //Check which module to display for project section
+    //chkProjSec();
+    
     $(window).resize(function(){
-        if($(window).width() < 768){
+        //chkProjSec();
+    });
+    
+    
+    $('a[href="#project"], a[href="#contact"]').on("click", function(e){
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+        return false;
+        
+        
+    });
+    
+});
+
+
+function chkProjSec(){
+    if($(window).width() < 768){
             $(".featurette").css("display", "none");
             $(".featurette-divider").css("display", "none");
             $("#myCarousel").css("display", "block");
@@ -10,9 +31,4 @@ $(document).ready(function(){
             $(".featurette-divider").css("display", "block");
             $("#myCarousel").css("display", "none");
         }
-    });
-    
-    
-    
-    
-});
+}
